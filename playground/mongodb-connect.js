6b8597f270f51fe8c47b8ba5fd37e84dda8eb285
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-12-04 23:27:33 +0700

    init git project

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Cannot connect to base ', err);
+  }
+  // db.collection('Todos').insertOne({
+  //   text: 'new message',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('error occurs ', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+  //
+  // db.collection('Users').insertOne({
+  //   name: 'Pavel',
+  //   age: 23,
+  //   location: 'Phuket'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('error occurs ', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // });
+  db.collection('Users').find().toArray().then((users) => {
+    console.log(users);
+  }, (error) => {
+    console.log('some error ', error);
+  });
+
+  console.log('Connected to base');
+  db.close();
+});
