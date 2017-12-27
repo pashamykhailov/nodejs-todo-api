36602dd95232e984b0f4d949725160ebb88139c4
Mykhailov Pavel <mykhailovpm@gmail.com>
2017-12-27 21:51:43 +0700

    add patch method

--- server/models/todo.js
+++ server/models/todo.js
@@ -9,7 +9,7 @@ let schema = new mongoose.Schema({
         type:Boolean,
         default: false
     },
-    completeAt: {
+    completedAt: {
         type: Number,
         default: null
     }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectId } = require("mongodb");
 
-const mongoose = require("./db/mongoose");
+const { mongoose } = require("./db/mongoose");
 const { User } = require("./models/user");
 const { Todo } = require("./models/todo");
 
@@ -74,6 +75,33 @@ app.delete("/todos/:id", (req, res) => {
       });
     })
     .catch((e) => res.status(400).send(e));
+});
+
+app.patch("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["text", "completed", "completedAt"]);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send("Not valid id");
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 
 app.listen(port, () => {
