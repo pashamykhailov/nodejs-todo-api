2658d8ef0330f35c8d697bdd95a9ce76ec890d82
Mykhailov Pavel <mykhailovpm@gmail.com>
2018-01-10 15:17:25 +0700

    add route for users

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 let users = new mongoose.Schema({
-    email: {
-        type: String,
-        minlength: 1,
-        trim: true,
-        required: true
+  email: {
+    type: String,
+    minlength: 1,
+    trim: true,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      msg: "{VALUE} is not valid email"
     }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
-let User = mongoose.model('User', users);
+let User = mongoose.model("User", users);
 
 module.exports = {
-    User
+  User
 };
--- server/server.js
+++ server/server.js
@@ -105,6 +105,16 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+  user.save().then((success) => {
+    res.send(success);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
